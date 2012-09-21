e42ba5d1d0b27d443b6b8aa14d3488afcc769261
Funet <funet@ubuntu.ubuntu-domain>
2012-09-21 14:41:05 +0800

    init vesion

---
+++ application/models/Step.php
@@ -0,0 +1,27 @@
+<?php
+
+class Step {
+    
+    // 重置处理订单流程
+    public static function reset() {
+        $current_step = Session::get('step', 'spiderOrder');
+        foreach(Config::get('application.steps') as $step => $item) {
+            if($current_step != $step) {
+                $new_item = [
+                    'name'  => $item['name'],
+                    'link'  => 'javascript:;',
+                    'class' => 'cantclick',
+                    'id'    => ''
+                    ];
+            } else {
+                $new_item = $item;
+            }
+
+            $new_steps[$step] = $new_item;
+        }
+        Config::set('application.steps', $new_steps);
+        
+    }
+
+}
+?>
