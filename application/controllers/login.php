1393b7ecdfeee64a4207cae4f611bbbfced6d670
funet <vipfu@163.com>
2012-10-02 17:07:19 +0800

    用户登录

---
+++ application/controllers/login.php
@@ -0,0 +1,29 @@
+<?php
+use Laravel\Redirect;
+
+use Laravel\Input;
+
+use Laravel\View;
+
+class Login_Controller extends Base_Controller
+{
+    public function action_index()
+    {
+        if (Sentry::check())
+        {
+            return Redirect::to('/');
+        }
+        return View::make('login');
+    }
+    
+    public function action_submit()
+    {
+        $username=Input::get('username');
+        $password=Input::get('password');
+        if(User::login($username, $password))
+        {
+            return Redirect::to('/');
+        }
+    }
+}
+?>
