c54fd0bb2b0576e658de9b79a731138f296de16a
funet <vipfu@163.com>
2012-10-03 16:41:08 +0800

    shipping

--- application/controllers/shipping.php
+++ application/controllers/shipping.php
@@ -1,5 +1,9 @@
 <?php
 
+use Laravel\Input;
+
+use Laravel\Redirect;
+
 class Shipping_Controller extends Base_Controller{
 
     public function action_index()
@@ -13,22 +17,25 @@ class Shipping_Controller extends Base_Controller{
             {
                 $return['status']='done';
                 $return['massage']='';
-                return Response::json($return);
+                //return Response::json($return);
             }
             else
             {
                 $return['status']='no_insert';
                 $return['massage']='没有插入任何信息';
-                return Response::json($return);
+                //return Response::json($return);
             }
         }
         else
         {
             $return=array('status'=>'data_not_match','message'=>'数据不正确');
-            return Response::json($return);
+            //return Response::json($return);
         }
+
+        return Redirect::to($_SERVER['HTTP_REFERER']);
+        
+        
     }
-
 }
-
+    
 ?>
