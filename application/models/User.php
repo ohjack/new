3462cb45218b016e995cbc1fda8f1c683ffc7928
funet <vipfu@163.com>
2012-10-08 10:09:42 +0800

    平台设置

--- application/models/User.php
+++ application/models/User.php
@@ -11,7 +11,7 @@ class User {
      */
     public static function getPlatforms( $user_id ) {
 
-        $fields = ['p.type', 'p.name', 'p.option', 'up.id', 'up.user_id', 'up.option as user_option'];
+        $fields = ['p.type', 'p.name', 'p.option', 'up.id', 'up.user_id', 'up.option as user_option','up.platform_id'];
 
         $platforms = DB::table('platform as p')->left_join('users_platform as up', 'p.id', '=', 'up.platform_id')
                                                ->where('up.user_id', '=', $user_id)
@@ -20,6 +20,75 @@ class User {
         return $platforms;
     }
     
+    /**
+     * 通过用户平台ID获取具体用户平台信息
+     * 
+     * @param  $userplatfrom_id
+     */
+    public static function getPlatform_one($userplatform_id)
+    {
+          $fields = ['p.type', 'p.name', 'p.option', 'up.id', 'up.user_id', 'up.option as user_option','up.platform_id'];
+
+       return $platforms = DB::table('platform as p')->left_join('users_platform as up', 'p.id', '=', 'up.platform_id')
+                                                     ->where('up.id','=',$userplatform_id)                                          
+                                                     ->first( $fields );
+    }
+    /**
+     * 配置用户平台信息
+     * @param 会员ID $user_id
+     * @param 配置数据 $option
+     * @param 平台ID  $platform_id
+     * 
+     * return void
+     */
+    public static function setPlatform($user_id,$platform_id,$option,$id=0)
+    {
+        if($id!=0)
+        {
+            //更新配置信息
+            self::modifyPlatform($id, $option);
+        }
+        else
+        {
+            //插入配置信息
+            $option['user_id']=$user_id;
+            $option['platform_id']=$platform_id;
+            self::insertPlatform($option);
+        }
+    }
+    
+    /**
+     * 添加平台配置
+     * @param 配置数据 $data
+     */
+    private static function insertPlatform($data)
+    {
+        DB::table('users_platform')->insert($data);
+    }
+    
+    /**
+     * 修改平台配置
+     * @param  $id 用户平台ID
+     * 
+     * @param  $option        平台数据
+     */
+    private static function modifyPlatform($id,$option)
+    {   
+
+        DB::table('users_platform')->where('id','=',$id)
+                                   ->update($option);
+    }
+    
+    /**
+     * 是否存在平台信息
+     * @param  $id        用户平台ID
+     * 
+     */
+    private static function existPlatform($id)
+    {
+        return DB::table('users_platform')->where('id','=',$user_id)
+                                          ->first();
+    }
     /*
      * 用户注册
      */
---
+++ application/views/platform/add.blade.php
@@ -0,0 +1,22 @@
+@layout('layout')
+@section('content')
+{{ Form::open('user/platformmod','POST')}}
+<table>
+    <tr>
+        <th>平台名称</th>
+        <td>{{$platform_name}}</td>
+    </tr>
+@foreach($option as $key=>$value)
+    <tr>
+        <th>{{$key}}</th>
+        <td><input name='{{$key}}' value='{{$value}}'></td>
+    </tr>
+@endforeach
+<input name='platform_id' type="hidden" value='{{$platform_id}}'>
+<input name='userplatform_id' type="hidden" value='{{$userplatform_id}}'>  
+    <tr><td><input type="submit" value='确定'></td><td></td></tr>
+</table>
+{{ Form::close()}}
+
+@endsection
+
