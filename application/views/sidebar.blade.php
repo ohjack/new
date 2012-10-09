cf5f34bc4ce14c707ffe17b389360050572f698f
funet <vipfu@163.com>
2012-10-09 15:42:15 +0800

    左侧二级菜单样式修改

--- application/views/sidebar.blade.php
+++ application/views/sidebar.blade.php
@@ -5,14 +5,15 @@
 position: absolute;
 background: url(../images/subArrow_2.png) no-repeat 10px 5px;
 font-size: 11px;
-width: 100px;
+width: 170px;
 top: 13px;
-padding-top: 10px;
-margin-left: 100px;
+
+margin-left: 96px;
+margin-top:-5px;
 display:none;
 }
 .SecMenuContent{
-width: 100px;
+width: 170px;
 border-bottom: 1px solid #343434;
 text-align: left;
 border-top: 1px solid #545454;
@@ -20,23 +21,28 @@ margin-top:-10px;
 margin-left:16px;
 background: url(../images/backgrounds/sidebar.jpg);
 padding: 0;
-padding-left:10px;
+-webkit-border-top-right-radius: 2px; -webkit-border-top-left-radius: 2px; -moz-border-radius-topright: 2px; -moz-border-radius-topleft: 2px; border-top: none; padding-top: 1px; 
+-webkit-border-bottom-right-radius: 2px; -webkit-border-bottom-left-radius: 2px; -moz-border-radius-bottomright: 2px; -moz-border-radius-bottomleft: 2px; 
 }
 
-.SecMenulink{
-width: 100px;
-height:28px
+.SecMenuLink{
+width: 170px;
+height:28px;
+border-bottom: 1px solid #343434 !important;
+border-top: 1px solid #545454 !important;
+
 }
 .SecMenuLink a{
-width: 100px;
+width: 170px;
 height:28px;
 line-height:28px;
 padding: 0 !important;
+padding-left:24px !important;
+background: url(../images/elements/control/subnav_arrow.png) no-repeat 12px 10px !important;
 text-decoration: none;
-color: #CCC !important;
-display: block;
 
 }
+
 </style>
 <script type="text/javascript">
 $(function(){
@@ -56,6 +62,13 @@ $(function(){
         <!-- Main nav -->
         <ul class="nav">
             <li><a href="{{ URL::base() }}" title="" class="active"><img src="{{ URL::base() }}/images/icons/mainnav/dashboard.png" alt="仪表盘" /><span>仪表盘</span></a>
+                <div class='SecMenu'>
+                    <div class='SecMenuContent'>
+                        <div class="SecMenuLink"><a href="#">设置用户平台</a></div>
+                        <div class="SecMenuLink"><a href="#">设置显示标识</a></div>
+                        <div class="SecMenuLink"><a href="#">修改密码</a></div>
+                    </div>
+                </div>
             </li>
             <li>
                 <a href="{{ URL::base() }}" title="" class="active"><img src="{{ URL::base() }}/images/icons/mainnav/tables.png" alt="仪表盘" /><span>设置2</span></a>
